e1bb6a5dbe93bd26bbd8dd96a8d0eab4c1b72634
Charles Joshua Uy <charlesjoshuauy@gmail.com>
2021-03-10 23:09:36 +0800

    add infinite to the animations

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,10 +1,10 @@
 function start_game() {
-  document.getElementById("rope").style.animation = "rope_down 3s ease-in-out"; 
-  document.getElementById("home_hangman").style.animation = "body_up 3s ease-in-out";
+  document.getElementById("rope").style.animation = "rope_down 3s ease-in-out infinite"; 
+  document.getElementById("home_hangman").style.animation = "body_up 3s ease-in-out infinite";
 
   setTimeout(() => {document.getElementById("home_hangman").src = "assets/images/home_hangman.png"; }, 1500);
   setTimeout(() => {redirect(); }, 3000);
-  setTimeout(() => {document.getElementById("home_hangman").src = "assets/images/home_hangman_cut.png"; }, 3000);
+  
 }
 
 function redirect() {
